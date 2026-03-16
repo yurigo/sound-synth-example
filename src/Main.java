21f377e7c76d3eff6e0a040c8a630cea5304e848
Yuri <ygonzoo@gmail.com>
2026-03-16 13:12:04 +0100

    chore: refactor WaveType intro inheritance

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        SoundSynth synth = new SoundSynth();
+
+        //SoundSynth synth = new SoundSynth();
+        SoundSynth sineSynth = new SoundSynthSine();
+        
+        // TODO:
+        // SoundSynth squareSynth = new SoundSynthSquare();
+        // SoundSynth triangleSynth = new SoundSynthTriange();
+        // SoundSynth sawtoothSynth = new SoundSynthSawtooth();
 
         try{
-            synth.makeSound(WaveType.SINE, 440, 1000);
-            synth.makeSound(WaveType.SINE, 440, 200);
-            synth.makeSound(WaveType.SINE, 700, 500);
-            synth.makeSound(WaveType.SINE, 440, 1000);
+            sineSynth.makeSound(440, 1000);
+            sineSynth.makeSound(440, 200);
+            sineSynth.makeSound(700, 500);
+            sineSynth.makeSound(440, 1000);
 
-            synth.makeSound(WaveType.SQUARE, 440, 1000);
-            synth.makeSound(WaveType.SQUARE, 440, 200);
-            synth.makeSound(WaveType.SQUARE, 700, 500);
-            synth.makeSound(WaveType.SQUARE, 440, 1000);
+            // TODO
+            // synth.makeSound(WaveType.SQUARE, 440, 1000);
+            // synth.makeSound(WaveType.SQUARE, 440, 200);
+            // synth.makeSound(WaveType.SQUARE, 700, 500);
+            // synth.makeSound(WaveType.SQUARE, 440, 1000);
 
-            synth.makeSound(WaveType.TRIANGLE, 440, 1000);
-            synth.makeSound(WaveType.TRIANGLE, 440, 200);
-            synth.makeSound(WaveType.TRIANGLE, 700, 500);
-            synth.makeSound(WaveType.TRIANGLE, 440, 1000);
+            // synth.makeSound(WaveType.TRIANGLE, 440, 1000);
+            // synth.makeSound(WaveType.TRIANGLE, 440, 200);
+            // synth.makeSound(WaveType.TRIANGLE, 700, 500);
+            // synth.makeSound(WaveType.TRIANGLE, 440, 1000);
 
-            synth.makeSound(WaveType.SAWTOOTH, 440, 1000);
-            synth.makeSound(WaveType.SAWTOOTH, 440, 200);
-            synth.makeSound(WaveType.SAWTOOTH, 700, 500);
-            synth.makeSound(WaveType.SAWTOOTH, 440, 1000);
+            // synth.makeSound(WaveType.SAWTOOTH, 440, 1000);
+            // synth.makeSound(WaveType.SAWTOOTH, 440, 200);
+            // synth.makeSound(WaveType.SAWTOOTH, 700, 500);
+            // synth.makeSound(WaveType.SAWTOOTH, 440, 1000);
         }
         catch(Exception e){
             System.out.println(e);
